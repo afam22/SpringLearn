bfbf3a13c6498766d99c229136581f6c109c8114
afam22 <happyguy_afam@yahoo.com>
2022-12-20 17:28:38 -0700

    Testing loose coupling using a dummy sort algorithm. It returns 3 when the spring boot application is run.

---
+++ src/main/java/com/famzy/springlearn/basics/BinarySearchImpl.java
@@ -0,0 +1,12 @@
+package com.famzy.springlearn.basics;
+
+public class BinarySearchImpl {
+
+    //creating a binary search method
+    public int binarySearch(int[] numbers, int numberToSearchFor) {
+        //Sorting an array
+        //Search the array
+        //Return the result
+        return 3;
+    }
+}
